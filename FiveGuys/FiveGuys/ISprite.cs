a3a62f465f7f280574892d20da35de5667b5590c
gengguangyuan <143434598+gengguangyuan@users.noreply.github.com>
2025-02-10 10:38:23 +0800

    Create ISprite.cs

---
+++ FiveGuys/FiveGuys/ISprite.cs
@@ -0,0 +1,12 @@
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace FiveGuys
+{
+  public interface ISprite
+  {
+    void Draw();
+    void Update();
+  }
+}
