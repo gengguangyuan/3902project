1de58831c32b3df5bfbba17b7515d6e1659c5d7d
gengguangyuan <143434598+gengguangyuan@users.noreply.github.com>
2025-02-10 08:58:23 +0800

    Create ICommand interface

---
+++ FiveGuys/FiveGuys/ICommand.cs
@@ -0,0 +1,16 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveGuys
+
+public interface ICommand
+{
+    void Execute();
+    void Undo();
+}
