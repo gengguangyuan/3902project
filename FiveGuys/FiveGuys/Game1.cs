7835f6ba03cf1f960d6d0ef737c58c4662ab55d7
Walid Suleiman <156993022+walidsuleiman@users.noreply.github.com>
2025-02-09 19:10:27 -0500

    Updated Project
    IController Interface and Keyboard and Mouse Controller implemented

---
+++ FiveGuys/FiveGuys/Game1.cs
@@ -0,0 +1,57 @@
+﻿using FiveGuys.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FiveGuys
+{
+    public class Game1 : Game
+    {
+        private MouseController mouseController;
+        private KeyboardController keyboardController;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D linkTexture;
+        FiveGuys.linkSprite LinkSprite;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            this._graphics.PreferredBackBufferHeight = 720;
+            this._graphics.PreferredBackBufferWidth = 1280;
+        }
+
+        protected override void Initialize()
+        {
+            keyboardController = new KeyboardController(this);
+            mouseController = new MouseController(this);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            linkTexture = Content.Load<Texture2D>("LinkFullSprite");
+        }
+        
+
+        protected override void Update(GameTime gameTime)
+        {
+            mouseController.Update();
+            keyboardController.Update();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.LightGoldenrodYellow);
+
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ FiveGuys/FiveGuys/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new FiveGuys.Game1();
+game.Run();
