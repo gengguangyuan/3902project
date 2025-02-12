e168aff497c699c646c901a5319484a4873ae507
walidsuleiman <suleiman.43@osu.edu>
2025-02-12 16:28:14 -0500

    retest

--- FiveGuys/ISprite.cs
+++ FiveGuys/ISprite.cs
@@ -6,8 +6,6 @@ namespace FiveGuys
 {
   public interface ISprite
   {
-        //test
-
     void Draw(SpriteBatch _spriteBatch, Vector2 position);
     void Update(GameTime gt);
     void LoadContent(ContentManager content);
